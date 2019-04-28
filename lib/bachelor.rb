ac097cd8aeaa84e82842c5ab652f80b6b383dae5
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-28 10:48:33 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,65 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  winner = ""
+  data.each do |name, persons|
+    if name == season
+      persons.each do |data|
+        if data["status"] == "Winner"
+          winner = data["name"].split[0]
+        end
+      end
+    end
+  end
+  winner
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  worker = ""
+  data.each do |name, persons|
+    persons.each do |data|
+      if data["occupation"] == occupation
+        worker = data["name"]
+      end
+    end
+  end
+  worker
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |name, persons|
+    persons.each do |data|
+      if data["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  occupation = []
+  data.each do |name, persons|
+    persons.each do |data|
+      if data["hometown"] == hometown
+        occupation.push(data["occupation"])
+      end
+    end
+  end
+  occupation[0]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  sum = 0
+  data.each do |name, persons|
+    if name == season
+      persons.each do |data|
+        ages.push(data["age"])
+      end
+    end
+  end
+  ages.map {|x| x.to_f}.each{|x| sum = sum + x}
+  (sum / ages.size).round
 end
